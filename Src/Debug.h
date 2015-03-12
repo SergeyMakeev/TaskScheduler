b95c980622a607d49a7787dd0a04ff7a400cafc2
Sergey Makeev <sergeymakeev@inbox.ru>
2015-03-12 19:39:54 +0300

    some posix changes

--- Src/Debug.h
+++ Src/Debug.h
@@ -1,6 +1,25 @@
 #pragma once
 
-#define REPORT_ASSERT( condition, description ) __debugbreak();
+
+
+#if _MSC_VER
+
+#define DEBUG_BREAK __debugbreak()
+
+#elif __GNUC__
+
+//#define DEBUG_BREAK raise(SIGTRAP)
+#define DEBUG_BREAK
+
+#else
+
+#error "Platform not specified"
+
+#endif
+
+
+
+#define REPORT_ASSERT( condition, description ) DEBUG_BREAK;
 
 #define ASSERT( condition, description ) { if ( !(condition) ) { REPORT_ASSERT( #condition, description ) } }
 #define VERIFY( condition, description, operation ) { if ( !(condition) ) { { REPORT_ASSERT( #condition, description ) }; operation; } }
--- Src/Platform.h
+++ Src/Platform.h
@@ -204,7 +204,7 @@ namespace MT
 		{
 		}
 
-		void Add(int sum) 
+		void Add(int sum)
 		{
 			InterlockedExchangeAdd(&value, sum);
 		}
@@ -251,10 +251,21 @@ namespace MT
 	{
 		LPTHREAD_START_ROUTINE pEntryPoint = (LPTHREAD_START_ROUTINE)entryPoint;
 
+		int res = 0;
+
+        pthread_attr_t threadAttr;
+        res = pthread_attr_init(&threadAttr);
+        ASSERT(res == 0, "Create thread error");
+
+        res = pthread_attr_setstacksize(&threadAttr, stackSize);
+        ASSERT(res == 0, "Create thread error");
+
 		pthread_t thread;
 
-        //int res = ::pthread_create( &thread, nullptr, stackSize, pEntryPoint, userData, CREATE_SUSPENDED, NULL );
-		return 0;
+        res = pthread_create(&thread, &threadAttr, pEntryPoint, userData);
+        ASSERT(res == 0, "Create thread error");
+
+		return thread;
 	}
 
 
