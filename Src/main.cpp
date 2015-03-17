5cd6b3bc8e1825569671f9613fc4f2b69d7c7ecd
Sergey Makeev <sergeymakeev@inbox.ru>
2015-03-17 12:17:47 +0300

    enable crt debug for memory leak detection

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,3 +1,7 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include <stdio.h>
 #include <string>
 #include <set>
@@ -10,7 +14,13 @@
 int main(int argc, char **argv)
 {
 	argc;	argv;
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+	_CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_DEBUG );
+
 	Tests::RunAll();
+
+	 _CrtDumpMemoryLeaks();
+
 	return 0;
 }
 
