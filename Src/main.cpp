786d6cc06de37ec487c2deccc4df89d7968b4d94
Sergey Makeev <sergeymakeev@inbox.ru>
2015-03-18 07:14:22 -0400

    Remove DbgCrt from POSIX

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,6 +1,10 @@
+#ifdef _WIN32
+
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
+
+#endif
 
 #include <stdio.h>
 #include <string>
@@ -14,12 +18,17 @@
 int main(int argc, char **argv)
 {
 	argc;	argv;
+
+#ifdef _WIN32
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 	_CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_DEBUG );
+#endif
 
 	Tests::RunAll();
 
+#ifdef _WIN32
 	 _CrtDumpMemoryLeaks();
+#endif
 
 	return 0;
 }
