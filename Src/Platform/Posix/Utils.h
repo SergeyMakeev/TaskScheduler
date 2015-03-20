6f3abf64037a3a0d40f83b92ab21d418171da3c5
bombomby <nb_grif@mail.ru>
2015-03-20 15:28:46 +0300

    Fixed windows compilation

--- Src/Platform/Posix/Utils.h
+++ Src/Platform/Posix/Utils.h
@@ -3,14 +3,14 @@
 
 namespace MT
 {
-	__inline uint64 GetTimeMicroSeconds()
+	__inline int64 GetTimeMicroSeconds()
 	{
 		struct timeval te;
 		gettimeofday(&te, NULL);
 		return te.tv_sec*1000000LL + te.tv_usec;
 	}
 
-	__inline uint64 GetTimeMilliSeconds()
+	__inline int64 GetTimeMilliSeconds()
 	{
 		return GetTimeMicroSeconds() / 1000;
 	}
