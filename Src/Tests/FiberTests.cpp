c6faa42eb808f99caf99a76199a88edfaa3fc0a0
Sergey Makeev <sergeymakeev@inbox.ru>
2015-03-20 19:46:03 +0300

    Add simple fibers test

---
+++ Src/Tests/FiberTests.cpp
@@ -0,0 +1,64 @@
+#include "Tests.h"
+#include "../../TestFramework/UnitTest++/UnitTest++.h"
+#include "../Scheduler.h"
+#include "../Platform.h"
+
+
+
+
+SUITE(FiberTests)
+{
+	int counter = 0;
+
+
+	MT::Fiber fiberMain;
+
+
+	void FiberFunc( void* userData )
+	{
+		CHECK_EQUAL(counter, 0);
+		counter++;
+
+		MT::Fiber* currentFiber = (MT::Fiber*)userData;
+		MT::Fiber::SwitchTo(*currentFiber, fiberMain);
+
+		CHECK_EQUAL(counter, 2);
+		counter++;
+
+		MT::Fiber::SwitchTo(*currentFiber, fiberMain);
+	}
+
+
+	void TestThread(void* userData)
+	{
+		counter = 0;
+
+		MT::Thread* thread = (MT::Thread*)userData;
+
+		fiberMain.CreateFromThread(*thread);
+
+		MT::Fiber fiber1;
+		fiber1.Create(16384, FiberFunc, &fiber1);
+
+		MT::Fiber::SwitchTo(fiberMain, fiber1);
+
+		CHECK_EQUAL(counter, 1);
+		counter++;
+
+		MT::Fiber::SwitchTo(fiberMain, fiber1);
+
+		CHECK_EQUAL(counter, 3);
+	}
+
+
+
+
+TEST(FiberSimpleTest)
+{
+	MT::Thread thread;
+	thread.Start(32768, TestThread, &thread);
+
+	thread.Stop();
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
