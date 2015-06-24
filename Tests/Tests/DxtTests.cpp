1f7f799ac3dda0e856d5be3cb7054313fa0e6597
Sergey Makeev <sergeymakeev@inbox.ru>
2015-06-24 13:41:29 +0300

    _kbhit  implementation for posix

--- Tests/Tests/DxtTests.cpp
+++ Tests/Tests/DxtTests.cpp
@@ -4,7 +4,45 @@
 
 #include <squish.h>
 #include <string.h>
+
+#ifdef _WIN32
+
 #include <conio.h>
+
+#else
+
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int _kbhit(void)
+{
+	struct termios oldt, newt;
+	int ch;
+	int oldf;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+	if(ch != EOF)
+	{
+		ungetc(ch, stdin);
+		return 1;
+	}
+
+	return 0;
+}
+#endif
 
 namespace EmbeddedImage
 {
